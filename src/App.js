f1773d159785382ce4747eb99c371923e779e8ee
amitsetty <amitsetty5@gmail.com>
2020-02-28 14:49:26 +0000

    comimt

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-const  Kekule  = require("kekule")
+const  {Kekule}  = require("kekule")
 
 function App() {
   console.log(Kekule)
   return (
     <div className="App">
-      <header className="App-header">
-    
-
-    
+      <header className="App-header">    
       </header>
     </div>
   );
